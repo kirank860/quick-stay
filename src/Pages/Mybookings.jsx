6a8b1c7817e2b1afe7431cf014066ac34af84eab
kirank860 <kkiru889@gmail.com>
2025-11-01 16:58:39 +0530

    finished room details page

---
+++ src/Pages/Mybookings.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from '../components/Title'
+
+const Mybookings = () => {
+  return (
+    <div>
+        <Title title="My Booking" subTitle=""/>
+    </div>
+  )
+}
+
+export default Mybookings
