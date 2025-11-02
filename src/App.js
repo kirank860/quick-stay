f8c35bfc07e0c8f41bd09d632a700b1045951829
kirank860 <kkiru889@gmail.com>
2025-11-02 23:56:12 +0530

    hotel owner page

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ import Footer from './components/Footer'
 import AllRooms from './Pages/AllRooms'
 import RoomDetails from './components/RoomDetails'
 import Mybookings from './Pages/Mybookings'
+import HotelReg from './components/HotelReg'
+import Layout from './Pages/hotelOwner/Layout'
 
 const App = () => {
   const isOwnerpath=useLocation().pathname.includes("owner")
   return (
     <div>
 { !isOwnerpath &&  <Navbar/>}
+{false && <HotelReg/>}
 <div className='min-h-[70vh]'>
    <Routes>
   <Route path='/' element={<Home/>}/>
   <Route path='/rooms' element={<AllRooms/>}/>
   <Route path='/rooms/:id' element={<RoomDetails/>}/>
   <Route path='/my-bookings' element={<Mybookings/>}/>
+  <Route path='/owner' element={<Layout/>}>
+
+  </Route>
    </Routes>
 </div>
 <Footer/>
